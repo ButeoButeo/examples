57a831f0d26672df207a1f3e7552e8e7d797fd34
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-01-22 23:28:39 +0100

    Blog app added

---
+++ miablog/admin/includes/footer.php
@@ -0,0 +1,19 @@
+	</div><!-- /.blog-main -->
+	</div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Mia's Blog &copy; 2015</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
+	</body>
+</html>
---
+++ miablog/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog');
+
+$site_description = 'Stvarnost se ne moze nicim promeniti, samo duhom. To sam vec otkrila. Da bi covek promenio stvari, mora ih samo drugacije sagledati. To nije nista. Svetlost mora da padne na njih. To je neshvatljiv kosmicki proces. Poznat mi je. Dozivela sam ga. Nazovi to ljubav i ja sam carobnjak.';
