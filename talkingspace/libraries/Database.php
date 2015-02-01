171e4888989de39959269e4322b01f906ea1fb7a
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-02-01 22:51:25 +0100

    Front page UI created from ts_html

--- talkingspace/libraries/Database.php
+++ talkingspace/libraries/Database.php
@@ -3,7 +3,7 @@ class Database {
 	private $host = DB_HOST;
 	private $user = DB_USER;
 	private $pass = DB_PASS;
-	private	$dbname = DB_NAME;
+	private $dbname = DB_NAME;
 	
 	private $dbh;
 	private $error;
@@ -11,76 +11,85 @@ class Database {
 	
 	public function __construct() {
 		// Set DSN
-		$dsn = 'mysql:host=' . $this->host . ';dbname=' .$this->dbname;
+		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
 		// Set options
-		$options = array(
+		$options = array (
 				PDO::ATTR_PERSISTENT => true,
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
 		);
-		// Create a new PDO instance
+		// Create a new PDO instanace
 		try {
 			$this->dbh = new PDO ($dsn, $this->user, $this->pass, $options);
-		} catch {
+		}		// Catch any errors
+		catch ( PDOException $e ) {
 			$this->error = $e->getMessage();
 		}
 	}
+	
 	
 	public function query($query) {
 		$this->stmt = $this->dbh->prepare($query);
 	}
 	
+	
 	public function bind($param, $value, $type = null) {
-		if (is_null($type)) {
-			switch(true) {
-				case is_init($value) :
+		if (is_null ( $type )) {
+			switch (true) {
+				case is_int ( $value ) :
 					$type = PDO::PARAM_INT;
 					break;
-				case is_bool($value) :
+				case is_bool ( $value ) :
 					$type = PDO::PARAM_BOOL;
 					break;
-				case is_null($value) :
-					$type = PDO::PARA_NULL;
+				case is_null ( $value ) :
+					$type = PDO::PARAM_NULL;
 					break;
-				case default :
+				default :
 					$type = PDO::PARAM_STR;
 			}
 		}
-		$this->stmt->bindValue($param, $value, $type);
+		$this->stmt->bindValue ( $param, $value, $type );
 	}
 	
-	public function execute() {
+	
+	public function execute(){
 		return $this->stmt->execute();
 	}
 	
-	public function resultset() {
+	
+	public function resultset(){
 		$this->execute();
 		return $this->stmt->fetchAll(PDO::FETCH_OBJ);
 	}
 	
-	public function single() {
+	
+	public function single(){
 		$this->execute();
 		return $this->stmt->fetch(PDO::FETCH_OBJ);
 	}
 	
-	public function rowCount() {
+	
+	public function rowCount(){
 		return $this->stmt->rowCount();
 	}
 	
-	public function lastInsertId() {
+	
+	public function lastInsertId(){
 		return $this->dbh->lastInsertId();
 	}
 	
 	
-	// ***
-	public function beginTransaction() {
+	public function beginTransaction(){
 		return $this->dbh->beginTransaction();
 	}
 	
-	public function endTransaction() {
+	
+	public function endTransaction(){
 		return $this->dbh->commit();
 	}
 	
-	public function cancelTransaction() {
+	
+	public function cancelTransaction(){
 		return $this->dbh->rollBack();
 	}
 }
