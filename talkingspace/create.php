4ee4f559f7eb903e76fd908ebe4fee54743a9460
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-02-01 23:44:42 +0100

    Rest of UI is created from folder ts_html

---
+++ talkingspace/create.php
@@ -0,0 +1,8 @@
+<?php require('core/init.php'); ?>
+
+<?php
+//Get Template & Assign Vars
+$template = new Template('templates/create.php');
+
+//Display template
+echo $template;
---
+++ talkingspace/topic.php
@@ -0,0 +1,8 @@
+<?php require('core/init.php'); ?>
+
+<?php
+//Get Template & Assign Vars
+$template = new Template('templates/topic.php');
+
+//Display template
+echo $template;
