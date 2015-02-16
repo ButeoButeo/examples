7e12f79bf6a1be7ff42e4623bfb5fbd01829b9ab
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-02-16 13:09:24 +0100

    Added logic for users to register (User class and Validator class created)

--- talkingspace/templates/register.php
+++ talkingspace/templates/register.php
@@ -31,6 +31,6 @@
 					<textarea id="about" rows="6" cols="80" class="form-control"
 					name="about" placeholder="Tell us about yourself (Optional)"></textarea>
 			</div>
-			<input name="register_submit" type="submit" class="btn btn-default" value="Register" />
+			<input name="register" type="submit" class="btn btn-default" value="Register" />
 </form>
 <?php include('includes/footer.php'); ?>
