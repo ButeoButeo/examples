67ffe1a8217b52198235a6402e44b5b1a561191d
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-02-21 00:39:14 +0100

    Display and add categories, data fetch from DB

--- talkingspace/templates/create.php
+++ talkingspace/templates/create.php
@@ -1,25 +1,23 @@
 <?php include('includes/header.php'); ?>	
-<form role="form">
+<form role="form" method = "post" action = "create.php">
 							<div class="form-group">
 								<label>Topic Title</label>
 								<input type="text" class="form-control" name="title" placeholder="Enter Post Title">
 							</div>
 							<div class="form-group">
 								<label>Category</label>
-								<select class="form-control">
-									<option>Design</option>
-									<option>Development</option>
-									<option>Business & Marketing</option>
-									<option>Search Engines</option>
-									<option>Cloud & Hosting</option>
-							</select>
+								<select class="form-control" name = "category">
+								<?php foreach(getCategories() as $category) : ?>
+									<option value = "<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+								<?php endforeach; ?>
+								</select>
 							</div>
 								<div class="form-group">
 									<label>Topic Body</label>
 									<textarea id="body" rows="10" cols="80" class="form-control" name="body"></textarea>
 									<script>CKEDITOR.replace('body');</script>
 								</div>
-							<button type="submit" class="btn btn-default">Submit</button>
+							<button name = "do_create" type="submit" class="btn btn-default">Submit</button>
 						</form>
 					</div>
 				</div>
