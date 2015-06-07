f861f3318c5b1a3e681fb5e841e44286179b1552
Andrea-Njegovan-Devtech <andrea.njegovan@devtechgroup.com>
2015-06-07 19:55:55 +0200

    Remove application

--- shoutit/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Connect to MySQL
-$con = mysqli_connect("localhost", "root", "", "shoutit");
-
-//Test Connection
-if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL" .mysqli_connect_error();
-} 
-?>
